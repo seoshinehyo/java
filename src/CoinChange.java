9c3667bb2cab2515b68fd3d516870a398ad889df
sshnote <springssh0504@naver.com>
2024-04-13 16:06:07 +0900

    알고리즘 과제

---
+++ src/CoinChange.java
@@ -0,0 +1,29 @@
+public class CoinChange {
+    // 202034723 컴퓨터공학전공 서상효
+    // 주어진 동전의 value를 담은 배열 S[]와 거슬러 줘야 할 금액 N이 주어졌을 때, 거스름돈을 구성하는 방법의 수를 반환하는 프로그램
+
+    public static void main(String[] args) {
+        int[] S = {1, 2, 3}; // 배열 S 선언
+        int m = S.length; // m은 배열 S의 크기
+        int N = 4; // 주어진 금액 N
+        System.out.println(count(S, m, N));
+    }
+
+    public static int count(int[] S, int m, int N) {
+        int[][] table = new int[N + 1][m]; // 중간 결과 저장하기 위한 크기가 N+1, m인 2차원 배열
+
+        // 테이블의 첫 번째 행 1로 초기화 -> 0을 만드는 방법은 정확히 하나
+        for (int i = 0; i < m; i++) {
+            table[0][i] = 1;
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            for (int j = 0; j < m; j++) {
+                int use = (i - S[j] >= 0) ? table[i - S[j]][j] : 0; // 현재 동전 S[j] 사용하여 금액 만드는 방법의 수
+                int notUse = (j >= 1) ? table[i][j - 1] : 0; // 현재 동전 사용하지 않고 이전 상태에서 금액 만드는 방법의 수
+                table[i][j] = use + notUse;
+            }
+        }
+        return table[N][m - 1]; // 최종 결과
+    }
+}
---
+++ src/ThreeWayMergeSort.java
@@ -0,0 +1,112 @@
+public class ThreeWayMergeSort {
+    // 202034723 서상효 Three-Way MergeSort를 사용해 정렬하는 프로그램
+
+    public static void main(String args[]) {
+        Integer[] S = new Integer[] {1, 3, 6, 2, 8, 5, 4, 9, 7}; // 정렬되지 않은 배열 S
+        mergeSort(S);
+        System.out.print("정렬된 배열 S : ");
+        for (int i = 0; i < S.length; i++) {
+            System.out.print(S[i] + " ");
+        }
+    }
+
+    public static void mergeSort(Integer[] S) {
+        if (S == null) {
+            return; // 배열이 비어있으면 종료
+        }
+
+        Integer[] U = new Integer[S.length]; // 배열 S 크기만큼의 배열 U 선언
+
+        for (int i = 0; i < U.length; i++) {
+            U[i] = S[i]; // 배열 copy
+        }
+
+        // sort function
+        mergeSortRec(U, 0, S.length, S);
+
+        for (int i = 0; i < U.length; i++)
+            S[i] = U[i]; // 정렬된 배열 U의 값들 배열 S로 copy
+    }
+    public static void mergeSortRec(Integer[] U, int low, int high, Integer[] V)
+    {
+        if (high - low < 2) {
+            return; // 배열 size가 1이면 종료
+        }
+
+        // mid1, mid2를 사용해 배열 3개로 Devide
+        int mid1 = low + ((high - low) / 3);
+        int mid2 = low + 2 * ((high - low) / 3) + 1;
+
+        // recursion을 사용해 각 배열 정렬
+        mergeSortRec(V, low, mid1, U);
+        mergeSortRec(V, mid1, mid2, U);
+        mergeSortRec(V, mid2, high, U);
+
+        merge(V, low, mid1, mid2, high, U); // 정렬된 3개의 배열 합치기
+    }
+
+    public static void merge(Integer[] S, int low, int mid1, int mid2, int high, Integer[] U)
+    {
+        int i = low, j = mid1, k = mid2, l = low;
+
+        // 3개의 배열 중 가장 작은 값 넣기
+        while ((i < mid1) && (j < mid2) && (k < high)) {
+            if (S[i].compareTo(S[j]) < 0) {
+                if (S[i].compareTo(S[k]) < 0) {
+                    U[l++] = S[i++];
+                } else {
+                    U[l++] = S[k++];
+                }
+            }
+            else {
+                if (S[j].compareTo(S[k]) < 0) {
+                    U[l++] = S[j++];
+                } else {
+                    U[l++] = S[k++];
+                }
+            }
+        }
+
+        // 첫번째, 두번째 배열 중 하나 끝날때까지 비교
+        while ((i < mid1) && (j < mid2)) {
+            if (S[i].compareTo(S[j]) < 0) {
+                U[l++] = S[i++];
+            } else {
+                U[l++] = S[j++];
+            }
+        }
+
+        // 두번째, 세번째 배열 중 하나 끝날때까지 비교
+        while ((j < mid2) && (k < high)) {
+            if (S[j].compareTo(S[k]) < 0) {
+                U[l++] = S[j++];
+            } else {
+                U[l++] = S[k++];
+            }
+        }
+
+        // 첫번째, 세번째 배열 비교
+        while ((i < mid1) && (k < high)) {
+            if (S[i].compareTo(S[k]) < 0) {
+                U[l++] = S[i++];
+            } else {
+                U[l++] = S[k++];
+            }
+        }
+
+        // 첫번째 배열에서 남는값 복사
+        while (i < mid1) {
+            U[l++] = S[i++];
+        }
+
+        // 두번째 배열에서 남는값 복사
+        while (j < mid2) {
+            U[l++] = S[j++];
+        }
+
+        // 세번째 배열에서 남는값 복사
+        while (k < high) {
+            U[l++] = S[k++];
+        }
+    }
+}
