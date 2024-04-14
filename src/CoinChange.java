89589cc20b4454548af44fc74cc4ddc253c99ffb
sshnote <springssh0504@naver.com>
2024-04-14 14:29:40 +0900

    CoinChange 코드 수정

--- src/CoinChange.java
+++ src/CoinChange.java
@@ -12,7 +12,7 @@ public class CoinChange {
     public static int count(int[] S, int m, int N) {
         int[][] table = new int[N + 1][m]; // 중간 결과 저장하기 위한 크기가 N+1, m인 2차원 배열
 
-        // 테이블의 첫 번째 행 1로 초기화 -> 0을 만드는 방법은 정확히 하나 -> 1
+        // 테이블의 첫 번째 행 1로 초기화 -> 0을 만드는 방법은 정확히 하나 -> 1로 초기화
         for (int i = 0; i < m; i++) {
             table[0][i] = 1;
         }
