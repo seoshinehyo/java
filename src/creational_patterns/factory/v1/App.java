2b16838f2facff748d5648298b9a7e57344d5214
sshnote <springssh0504@naver.com>
2024-05-12 19:55:16 +0900

    App 수정

--- src/creational_patterns/factory/v1/App.java
+++ src/creational_patterns/factory/v1/App.java
@@ -2,7 +2,7 @@ package creational_patterns.factory.v1;
 
 public class App {
     public static void main(String[] args) {
-        Vehicle v = VehicleFactory.createNew(Vtype.CAR);
+        Vehicle v = VehicleFactory.createNew(Vtype.CAR); // enum type에 따라 달라짐
         v.go();
     }
 }
