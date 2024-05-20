9d704171b28875c96e3c8741af1aa793d76b692a
sshnote <springssh0504@naver.com>
2024-05-20 18:40:48 +0900

    Factory 수정

--- src/creational_patterns/factory/v1/VehicleFactory.java
+++ src/creational_patterns/factory/v1/VehicleFactory.java
@@ -16,7 +16,7 @@ public class VehicleFactory {
 //            case TRUCK -> new Truck();
 //        };
 
-        return switch (type) { // Factory에서 리턴
+        return switch (type) { // Factory에서 타입에 맞게 리턴
             case CAR -> new Car();
             case TRUCK -> new Truck();
             case BUS -> new Bus();
