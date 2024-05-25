40e4337eb2ae74bb142460a32619ce5300953dff
sshnote <springssh0504@naver.com>
2024-05-25 17:53:53 +0900

    item 정렬 코드 추가

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -10,7 +10,7 @@ public class Knapsack {
     static int[] include; // 지금까지 어떤 물건 선택했는지("yes"면 선택, "no"면 건너뛴 item)
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\82105\\Desktop\\SSH\\3-1\\소웨공\\SWE\\src\\instance.txt")); // 파일 읽기
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\82105\\Desktop\\SSH\\3-1\\소웨공\\SWE\\src\\instance3.txt")); // 파일 읽기
 
         // 첫 번째 줄 : # of items - n, maximum weight - W
         String[] firstLine = reader.readLine().split(" ");
@@ -36,11 +36,38 @@ public class Knapsack {
         bestSet = new int[n];
         include = new int[n];
 
+        // 아이템들을 pi/wi 비율에 따라 정렬
+        sortItems(w, p, n);
+
         knapsack(0, 0, 0, w, p, W, n); // index, profit, weight의 초기값은 전부 0
 
         // 결과 출력
         System.out.println("Optimal solution: " + Arrays.toString(bestSet).replaceAll("[\\[\\],]", ""));
         System.out.println("Maximum profit: " + maxProfit);
+    }
+
+    private static void sortItems(int[] w, int[] p, int n) {
+        Double[] ratios = new Double[n]; // pi/wi 값을 담을 배열
+        Integer[] indices = new Integer[n]; // 원래 배열의 인덱스를 담는 배열
+        for (int i = 0; i < n; i++) {
+            ratios[i] = (double) p[i] / w[i];
+            indices[i] = i;
+        }
+
+        // 람다식을 사용해 ratios를 기준으로 indices 정렬
+        Arrays.sort(indices, (i, j) -> Double.compare(ratios[j], ratios[i]));
+
+        int[] sortedW = new int[n]; // 정렬된 W
+        int[] sortedP = new int[n]; // 정렬된 P
+
+        for (int i = 0; i < n; i++) {
+            sortedW[i] = w[indices[i]];
+            sortedP[i] = p[indices[i]];
+        }
+
+        // 정렬된 배열을 원래 배열에 복사
+        System.arraycopy(sortedW, 0, w, 0, n);
+        System.arraycopy(sortedP, 0, p, 0, n);
     }
 
     // Backtracking Method
