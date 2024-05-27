70f9ae69ef7f15db3314ee34d15e0be6c20132e6
sshnote <springssh0504@naver.com>
2024-05-27 20:24:54 +0900

    knapsack 최종본

--- src/Knapsack.java
+++ src/Knapsack.java
@@ -7,10 +7,11 @@ public class Knapsack {
     static int maxProfit = 0; // 지금까지의 best값
     static int numBest = 0; // best 달성했을때의 레벨
     static int[] bestSet; // best 만드는 set 저장
-    static int[] include; // 지금까지 어떤 물건 선택했는지("yes"면 선택, "no"면 건너뛴 item)
+    static int[] include; // 지금까지 어떤 물건 선택했는지(1이면 선택, 2면 건너뛴 item)
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\82105\\Desktop\\SSH\\3-1\\소웨공\\SWE\\src\\instance3.txt")); // 파일 절대 경로로 읽기
+        // 파일 절대 경로로 읽기
+        BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\82105\\Desktop\\SSH\\3-1\\소웨공\\SWE\\src\\instance.txt"));
 
         // 첫 번째 줄 : # of items - n, maximum weight - W
         String[] firstLine = reader.readLine().split(" ");
@@ -42,7 +43,7 @@ public class Knapsack {
         knapsack(0, 0, 0, w, p, W, n); // index, profit, weight의 초기값은 전부 0
 
         // 결과 출력
-        System.out.println("Optimal solution: " + Arrays.toString(bestSet).replaceAll("[\\[\\],]", "")); // 출력 형식에 맞게 변경
+        System.out.println("Optimal solution: " + Arrays.toString(bestSet).replaceAll("[\\[\\],]", ""));
         System.out.println("Maximum profit: " + maxProfit); // Maximum profit값 출력
     }
 
@@ -57,8 +58,8 @@ public class Knapsack {
         // 람다식을 사용해 ratios를 기준으로 indices 정렬
         Arrays.sort(indices, (i, j) -> Double.compare(ratios[j], ratios[i]));
 
-        int[] sortedW = new int[n]; // 정렬된 W
-        int[] sortedP = new int[n]; // 정렬된 P
+        int[] sortedW = new int[n]; // 정렬된 w
+        int[] sortedP = new int[n]; // 정렬된 p
 
         for (int i = 0; i < n; i++) {
             sortedW[i] = w[indices[i]];
@@ -79,8 +80,10 @@ public class Knapsack {
         }
 
         if (promising(i, profit, weight, w, p, W, n)) {
-            include[i] = 1; // 해당 item 포함하면 1, include 배열의 0번째 index부터 값을 넣어야 되므로 include[i + 1]이 아닌 include[i] 사용
-            // 왼쪽 노드, i + 1을 불러 recursion을 하는 것이 맞는데, 배열 p에 0번째 요소부터 profit 값이 들어있으므로 ppt처럼 p[i + 1]과 w[i + 1]을 더하는 것이 아닌 p[i]값과 w[i]값을 더해주는 것이 맞음
+            // 해당 item 포함하면 1, include 배열의 0번째 index부터 값을 넣어야 되므로 include[i + 1]이 아닌 include[i] 사용
+            include[i] = 1;
+            // 왼쪽 노드, i + 1을 불러 recursion을 하는 것이 맞는데, 배열 p에 0번째 요소부터 profit 값이 들어있으므로 ppt처럼
+            // p[i + 1]과 w[i + 1]을 더하는 것이 아닌 p[i]값과 w[i]값을 더해주는 것이 맞음
             knapsack(i + 1, profit + p[i], weight + w[i], w, p, W, n);
             include[i] = 0; // 해당 item 포함하지 않으면 0
             knapsack(i + 1, profit, weight, w, p, W, n); // 해당 item 포함하지 않고 오른쪽 노드로
