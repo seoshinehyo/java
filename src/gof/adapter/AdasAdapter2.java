cdd0bc74697b5bd1a7e0d4c92e3deb080e2b223c
sshnote <springssh0504@naver.com>
2024-06-02 20:14:08 +0900

    주석 추가

--- src/gof/adapter/AdasAdapter2.java
+++ src/gof/adapter/AdasAdapter2.java
@@ -5,7 +5,7 @@ public class AdasAdapter2 implements ModuleIF{ // 방법2 상속 안 받고, 위
     ADASModule adas = new ADASModule();
 
     @Override
-    public void connect() { // connect() 요청이 왔을때 open() 호출해줌
+    public void connect() { // connect() 요청이 왔을때 open() 호출
         adas.open();
     }
 }
