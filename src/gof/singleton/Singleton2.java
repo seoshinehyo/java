f17e5ea0ec5db991392258ff1384affa9eb8bbb8
sshnote <springssh0504@naver.com>
2024-06-05 00:03:48 +0900

    Static inner

--- src/gof/singleton/Singleton2.java
+++ src/gof/singleton/Singleton2.java
@@ -1,10 +1,10 @@
 package gof.singleton;
 
-public class Singleton2 {
+public class Singleton2 { // Static inner
     private Singleton2() {}
 
-    private static class SingletonHolder {
-        private static final Singleton2 INSTANCE = new Singleton2();
+    private static class SingletonHolder { // SingletonHolder 클래스
+        private static final Singleton2 INSTANCE = new Singleton2(); // static inner로 안전하게 사용
     }
 
     public static Singleton2 getInstance() {
