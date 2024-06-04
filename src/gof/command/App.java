6b5f358ba05193d7c7e0c7563e00d24cf4a1aff0
sshnote <springssh0504@naver.com>
2024-06-04 19:46:18 +0900

    컨트롤러로 여러 조합 만들기

--- src/gof/command/App.java
+++ src/gof/command/App.java
@@ -3,7 +3,7 @@ package gof.command;
 public class App {
     public static void main(String[] args) {
         Invoker controller1 = new Invoker(); // 컨트롤러 만들기
-        // off를 wiper 로 바꿔서도 해봄. 조합 가능
+        // off를 wiper 로 바꿔서도 해봄. 조합 가능,  여러가지 조합 가능
         controller1.setCommand(new OnCommand(new HeaterReceiver()),new OffCommand(new WiperReceiver()));
 
         Invoker controller2 = new Invoker();
